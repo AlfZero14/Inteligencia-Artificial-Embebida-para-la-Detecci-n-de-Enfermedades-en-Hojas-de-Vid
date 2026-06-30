489cff22090ad6c5655e5cff42d248718d556b23
Javier12341 <160994556+Javier12341@users.noreply.github.com>
2026-06-29 19:34:18 -0500

    Add files via upload

---
+++ camera_pins.h
@@ -0,0 +1,24 @@
+// camera_pins.h - Definición de pines para ESP32-S3 Eye
+
+#ifndef CAMERA_PINS_H
+#define CAMERA_PINS_H
+
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM     10
+#define SIOD_GPIO_NUM     39
+#define SIOC_GPIO_NUM     38
+
+#define Y9_GPIO_NUM       48
+#define Y8_GPIO_NUM       47
+#define Y7_GPIO_NUM       46
+#define Y6_GPIO_NUM       45
+#define Y5_GPIO_NUM       44
+#define Y4_GPIO_NUM       43
+#define Y3_GPIO_NUM       42
+#define Y2_GPIO_NUM       41
+#define VSYNC_GPIO_NUM    40
+#define HREF_GPIO_NUM     39
+#define PCLK_GPIO_NUM     39
+
+#endif
